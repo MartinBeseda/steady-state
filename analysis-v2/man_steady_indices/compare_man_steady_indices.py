4bad24dc7a88a9332786be8f6730fd8a9a4e332b
Martin Beseda <martinbeseda@seznam.cz>
2024-11-07 02:20:56 +0100

    [ENH] Added two more manual labelings into man_steady_indices.

--- analysis-v2/man_steady_indices/compare_man_steady_indices.py
+++ analysis-v2/man_steady_indices/compare_man_steady_indices.py
@@ -5,25 +5,30 @@
 import json
 import simpleJDB
 import matplotlib.pyplot as plt
+import itertools
 
 
 # Load the data
 data_vittorio = simpleJDB.database('benchmark_database_steady_indices_vittorio')
 data_michele = simpleJDB.database('benchmark_database_steady_indices_michele')
 data_daniele = simpleJDB.database('benchmark_database_steady_indices_daniele')
+data_luca = simpleJDB.database('benchmark_database_steady_indices_luca')
+data_martin = simpleJDB.database('benchmark_database_steady_indices_martin')
 
 data_sum = {}
 
 for i, e in enumerate(data_vittorio.data['main']):
-    print(e['value'], data_michele.getkey(e['keyname']), data_daniele.getkey(e['keyname']))
+    print(e['value'], data_michele.getkey(e['keyname']), data_daniele.getkey(e['keyname']),
+          data_luca.getkey(e['keyname']), data_martin.getkey(e['keyname']))
 
     # Remove the bad suffix
     fname = e['keyname'].rsplit('_', 1)[0]
     data_sum[e['keyname']] = {'idxs': [e['value'],
                                        data_michele.getkey(e['keyname']),
-                                       data_daniele.getkey(e['keyname'])],
+                                       data_daniele.getkey(e['keyname']),
+                                       data_luca.getkey(e['keyname']),
+                                       data_martin.getkey(e['keyname'])],
                               'series': json.load(open(f'data_st/{fname}'))}
-
 
     # # Create plots
     # plt.figure()
@@ -31,53 +36,43 @@ for i, e in enumerate(data_vittorio.data['main']):
     # plt.vlines([data_sum[e['keyname']]['idxs']],
     #            min(data_sum[e['keyname']]['series']),
     #            max(data_sum[e['keyname']]['series']),
-    #            colors=['red', 'green', 'blue'])
+    #            colors=['red', 'green', 'blue', 'yellow', 'orange'])
     # plt.title(fname.replace('#', ' '), wrap=True)
     # plt.savefig(f'plots/plot_{i}.png')
     # plt.close()
 
-# Create histograms
-diffs_1_2 = []
-diffs_2_3 = []
-diffs_1_3 = []
-for k, e in data_sum.items():
-    diffs_1_2.append(e['idxs'][0] - e['idxs'][1])
-    diffs_2_3.append(e['idxs'][1] - e['idxs'][2])
-    diffs_1_3.append(e['idxs'][0] - e['idxs'][2])
+# Data indices
+data_idxs = list(itertools.combinations(range(5), 2))
 
-# plt.figure()
-# plt.hist(diffs_1_2)
-# plt.savefig('histograms/diffs_1_2.png')
-# plt.title('Differences idx 0 - idx 1')
-# plt.close()
-#
-# plt.figure()
-# plt.hist(diffs_2_3)
-# plt.savefig('histograms/diffs_2_3.png')
-# plt.title('Differences idx 1 - idx 2')
-# plt.close()
-#
-# plt.figure()
-# plt.hist(diffs_1_3)
-# plt.savefig('histograms/diffs_1_3.png')
-# plt.title('Differences idx 1 - idx 3')
-# plt.close()
-#
-# plt.figure()
-# plt.hist([diffs_1_2, diffs_1_3, diffs_2_3], label=['0-1', '0-2', '1-2'])
-# plt.title('Differences (0 - 1, 0 - 2, 1 - 2)')
-# plt.legend()
-# plt.savefig('histograms/all_diffs.png')
-# plt.close()
+# Create histograms
+diffs = {f'{e[0]}-{e[1]}': [] for e in data_idxs}
+
+for k, e in data_sum.items():
+    for idx_pair in data_idxs:
+        diffs[f'{idx_pair[0]}-{idx_pair[1]}'].append(e['idxs'][idx_pair[0]] - e['idxs'][idx_pair[1]])
+
+for k, e in diffs.items():
+    plt.figure()
+    plt.title(f'Differences idx {k}')
+    plt.hist(e)
+    plt.savefig(f'histograms/diffs_{k}.png')
+    plt.close()
 
 plt.figure()
-plt.hist([diffs_1_2, diffs_1_3, diffs_2_3], label=['0-1', '0-2', '1-2'], cumulative=True)
-plt.title('Cumulative differences (0 - 1, 0 - 2, 1 - 2)')
+plt.hist(diffs.values(), label=diffs.keys())
+plt.title('SSD Differences')
+plt.legend()
+plt.savefig('histograms/all_diffs.png')
+plt.close()
+
+plt.figure()
+plt.hist(diffs.values(), label=diffs.keys(), cumulative=True)
+plt.title('SSD Cumulative differences')
 plt.legend()
 plt.savefig('histograms/all_diffs_cumulative.png')
 plt.close()
 
-# # Create boxplots of differences' variance
-# plt.boxplot([diffs_1_2, diffs_1_3, diffs_2_3], tick_labels=['0-1', '0-2', '1-2'])
-# plt.savefig('boxplots/boxplot_all_diffs.png')
-# plt.close()
+# Create boxplots of differences' variance
+plt.boxplot(diffs.values(), tick_labels=diffs.keys())
+plt.savefig('boxplots/boxplot_all_diffs.png')
+plt.close()
